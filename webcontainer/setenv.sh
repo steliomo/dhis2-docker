0eb5b38e4281f90c3af4f079555097abd9d2d9e2
steliomo <steliom@gmail.com>
2022-10-15 10:13:57 +0200

    memory fix

--- webcontainer/setenv.sh
+++ webcontainer/setenv.sh
@@ -1,2 +1,2 @@
-export JAVA_OPTS="-Dfile.encoding=UTF-8 -server -Xms512m -Xmx1536m -XX:PermSize=256m -XX:MaxPermSize=768m -Duser.timezone=Africa/Maputo"
+export JAVA_OPTS="-Dfile.encoding=UTF-8 -server -Xms4096m -Xmx12288m -XX:PermSize=256m -XX:MaxPermSize=768m -Duser.timezone=Africa/Maputo"
 export DHIS2_HOME='/home/dhis/config'
