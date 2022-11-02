2657fab0e7bc6c6958d438459d2578ab2664727d
steliomo <steliom@gmail.com>
2022-11-02 16:08:23 +0200

    database tuning

--- webcontainer/setenv.sh
+++ webcontainer/setenv.sh
@@ -1,2 +1,2 @@
-export JAVA_OPTS="-Dfile.encoding=UTF-8 -server -XX:InitialRAMPercentage=3 -XX:MaxRAMPercentage=85 -Duser.timezone=Africa/Maputo"
+export JAVA_OPTS="-Dfile.encoding=UTF-8 -server -XX:InitialRAMPercentage=3 -XX:MaxRAMPercentage=80 -Duser.timezone=Africa/Maputo"
 export DHIS2_HOME='/home/dhis/config'
